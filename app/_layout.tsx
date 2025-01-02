6637bf945e8802dd140572dcbf54ceb2a2b098d0
Abel <abel.diazv64@gmail.com>
2025-01-01 19:14:42 -0600

    Tab bar implementation part 1

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,32 @@
-import { StyleSheet, Text, View } from 'react-native'
-import { Slot, Stack } from 'expo-router';
-import "../global.css"
+import { StyleSheet, Text, View } from 'react-native';
+import { Slot, SplashScreen, Stack } from 'expo-router';
+import "../global.css";
+import { useFonts } from 'expo-font';
+import { useEffect } from 'react';
+
+SplashScreen.preventAutoHideAsync();
 
 const RootLayout = () => {
+  const [fontsLoaded, error] = useFonts({
+    "Poppins-Black": require("../assets/fonts/Poppins-Black.ttf"),
+    "Poppins-Bold": require("../assets/fonts/Poppins-Bold.ttf"),
+    "Poppins-ExtraBold": require("../assets/fonts/Poppins-ExtraBold.ttf"),
+    "Poppins-ExtraLight": require("../assets/fonts/Poppins-ExtraLight.ttf"),
+    "Poppins-Light": require("../assets/fonts/Poppins-Light.ttf"),
+    "Poppins-Medium": require("../assets/fonts/Poppins-Medium.ttf"),
+    "Poppins-Regular": require("../assets/fonts/Poppins-Regular.ttf"),
+    "Poppins-SemiBold": require("../assets/fonts/Poppins-SemiBold.ttf"),
+    "Poppins-Thin": require("../assets/fonts/Poppins-Thin.ttf"),
+  }); //Hooks are funcitons that let you hook into React state
+
+  // useEffects allow you to permform action while screen is loading
+  useEffect(() => {
+    if (error) throw error;
+    if(fontsLoaded) SplashScreen.hideAsync();
+  }, [fontsLoaded, error]) // dependency array 
+  // (recall this function whenever fontsLoaded change or when there is an error)
+    if(!fontsLoaded && !error) return null;
+
   return (
     <Stack>
       <Stack.Screen name ="index" options={{
